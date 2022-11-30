216b0a6380e2ef312aec3ce2fce1eb84a9987d20
Guilherme Cossari <guilhermecossarimattos@gmail.com>
2022-11-30 20:11:48 -0300

    Component Notificação(Toggle)

--- src/Pages/HabitPages/index.jsx
+++ src/Pages/HabitPages/index.jsx
@@ -6,20 +6,22 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
-  Alert
+  Alert,
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
+import Notification from "../../Components/HabitPage/Notification";
 
 export default function HabitPage({ route }) {
-	  const navigation = useNavigation();
-    const [habitInput, setHabitInput] = useState();
-    const [frequencyInput, setFrequencyInput] = useState();
+  const navigation = useNavigation();
+  const [habitInput, setHabitInput] = useState();
+  const [frequencyInput, setFrequencyInput] = useState();
+  const [notificationToggle, setNotificationToggle] = useState();
 
-    const { create, habit } = route.params;
+  const { create, habit } = route.params;
 
   return (
     <View style={styles.container}>
@@ -43,12 +45,19 @@ export default function HabitPage({ route }) {
 
             <Text style={styles.inputText}>Hábito</Text>
             <SelectHabit habit={habit} habitInput={setHabitInput} />
-            
+
             <Text style={styles.inputText}>Frequência</Text>
             <SelectFrequency
               habitFrequency={habit?.habitFrequency}
               frequencyInput={setFrequencyInput}
             />
+
+            {frequencyInput === "Mensal" ? null : (
+              <Notification
+                notificationToggle={notificationToggle}
+                setNotificationToggle={setNotificationToggle}
+              />
+            )}
           </View>
         </View>
       </ScrollView>
@@ -57,45 +66,45 @@ export default function HabitPage({ route }) {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "rgba(21, 21, 21, 0.98)",
-    },
-    bakcPageBtn: {
-      width: 40,
-      height: 40,
-      margin: 25,
-    },
-    arrowBack: {
-      width: 40,
-      height: 40,
-    },
-    mainContent: {
-      width: 250,
-      alignSelf: "center",
-    },
-    title: {
-      fontWeight: "bold",
-      textAlign: "center",
-      color: "white",
-      fontSize: 30,
-    },
-    inputText: {
-      color: "white",
-      fontSize: 16,
-      marginTop: 35,
-      marginBottom: 10,
-      marginLeft: 5,
-    },
-    inputContainer: {
-      borderWidth: 1,
-      borderColor: "#FFFFFF",
-      borderRadius: 10,
-      paddingHorizontal: 20,
-      paddingVertical: 15,
-    },
-    area: {
-      color: "#BBBBBB",
-      fontSize: 15,
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  bakcPageBtn: {
+    width: 40,
+    height: 40,
+    margin: 25,
+  },
+  arrowBack: {
+    width: 40,
+    height: 40,
+  },
+  mainContent: {
+    width: 250,
+    alignSelf: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+  },
+  inputText: {
+    color: "white",
+    fontSize: 16,
+    marginTop: 35,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  area: {
+    color: "#BBBBBB",
+    fontSize: 15,
+  },
+});
